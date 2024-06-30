aadd8e33e85ca4d3785bc44540cbfc626766655c
med.wf <med.wf95@gmail.com>
2024-06-30 11:35:52 +0100

    adding return to responce

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,20 +6,19 @@ class UsersController {
     const { email, password } = req.body;
 
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
 
     const user = await dbClient.User('get', email, password);
     if (user) {
-      res.status(400).json({ error: 'Already exist' });
-    } else {
-      const hashedPassword = sha1(password);
-      const response = await dbClient.User('set', email, hashedPassword);
-      res.status(201).json({ id: response.insertedId, email });
+      return res.status(400).json({ error: 'Already exist' });
     }
+    const hashedPassword = sha1(password);
+    const response = await dbClient.User('set', email, hashedPassword);
+    return res.status(201).json({ id: response.insertedId, email });
   }
 }
 
